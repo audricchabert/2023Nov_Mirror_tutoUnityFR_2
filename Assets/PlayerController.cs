31b6ab6cc33ad4ee739ed4d454da36fa2622fb46
achabert <pcmgs33@gmail.com>
2023-11-13 16:03:09 +0100

    first episode : end of episode

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+//RequireComponent fait en sorte qu'on ne puisse pas supprimer un component dépendant .via l'inspecteur
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    //SerializeField fait en sorte que la variable soit visible et modifiable depuis l'inspecteur, au niveau du script
+    [SerializeField]
+    private float speed;
+
+    private PlayerMotor playerMotor;
+
+    private void Start()
+    {
+        playerMotor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        // Calculer la vélocité du mouvement du joueur
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+        
+    }
+}
