d52477fffc718fb725aa8eacb3024ac5659648c9
achabert <pcmgs33@gmail.com>
2023-11-20 17:08:15 +0100

    episode 2 : end of episode 2

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,7 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     //SerializeField fait en sorte que la variable soit visible et modifiable depuis l'inspecteur, au niveau du script
     [SerializeField]
-    private float speed;
+    private float speed = 3f;
+
+    [SerializeField]
+    private float mouseSensitivityX = 3f;
+    [SerializeField]
+    private float mouseSensitivityY = 3f;
 
     private PlayerMotor playerMotor;
 
@@ -17,7 +22,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Calculer la vélocité du mouvement du joueur
+        // Calculer la vélocité du mouvement du joueur (avant-arrière et sur les côtés)
         float xMov = Input.GetAxisRaw("Horizontal");
         float zMov = Input.GetAxisRaw("Vertical");
 
@@ -25,6 +30,22 @@ public class PlayerController : MonoBehaviour
         Vector3 moveVertical = transform.forward * zMov;
 
         Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
-        
+
+        playerMotor.MovePlayer(velocity);
+
+        //Calculer la rotation du joueur (pour regarder sur les côtés : donc lateralRotation)
+        //note : on utilise le mouvement de la souris sur l'axe X (mouse X) pour associer une rotation du joueur sur l'axe y (yRot)
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        // sur ce vector, on met bien yRot à la position Y du vecteur (les 3 valeurs du vecteurs devraient correspondre respectivement à X, Y, Z dans cet ordre)
+        Vector3 lateralRotation = new Vector3(0, yRot, 0) * mouseSensitivityX;
+
+        playerMotor.RotatePlayer(lateralRotation);
+
+        //Calculer la rotation du joueur (pour regarder en haut en bas )
+        //note : on utilise le mouvement de la souris sur l'axe Y (mouse Y) pour associer une rotation du joueur sur l'axe x (xRot)
+        float xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 verticalRotation = new Vector3(xRot, 0, 0) * mouseSensitivityY;
+        playerMotor.RotateCamera(verticalRotation);
     }
 }
--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -3,5 +3,63 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
+    private Vector3 internalVelocity;
+    private Vector3 internalRotation;
+    private Vector3 internalVerticalRotation;
 
+    private Rigidbody playerRigidbody;
+
+    [SerializeField]
+    private Camera playerCamera;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    //this method is just used to set the desired movement value. the movement will be processed in PerformMovement
+    public void MovePlayer(Vector3 _velocity)
+    {
+        internalVelocity = _velocity;
+    }
+
+
+    //this method is just used to set the desired rotation value. the movement will be processed in PerformRotation
+    public void RotatePlayer(Vector3 _rotation)
+    {
+        internalRotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _verticalRotation)
+    {
+        internalVerticalRotation = _verticalRotation;
+    }
+
+    private void PerformMovement()
+    {
+        //only perform the movement when the velocity is not null
+        if(internalVelocity != Vector3.zero)
+        {
+            //todo : check if its not better to use rigidbody.AddForce instead of MovePosition
+            playerRigidbody.MovePosition(playerRigidbody.position + internalVelocity * Time.fixedDeltaTime);
+            
+        }
+    }
+
+    private void PerformRotation()
+    {
+        //perform the lateral/horizontal rotation using the rigidbody
+        playerRigidbody.MoveRotation(playerRigidbody.rotation * Quaternion.Euler(internalRotation));
+
+
+        //perform the vertical rotation using the camera
+        playerCamera.transform.Rotate(-internalVerticalRotation);
+
+    }
 }
