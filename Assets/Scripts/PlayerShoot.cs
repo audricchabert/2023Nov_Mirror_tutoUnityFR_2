995dfcefb6a3974cad4a478656787adc8d964e2f
achabert <pcmgs33@gmail.com>
2023-12-11 16:45:51 +0100

    episode 6 : @16:10 : working raycast

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    public PlayerWeapon playerWeapon;
+
+    [SerializeField]
+    private Camera playerCamera;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(playerCamera == null)
+        {
+            Debug.LogError("Pas de caméra renseignée sur PlayerShoot");
+            this.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+
+        if( Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, playerWeapon.range, mask))
+        {
+            Debug.Log("object hit : " + hit.collider.name);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,7 @@
+﻿[System.Serializable]
+public class PlayerWeapon
+{
+    public string name = "default weapon";
+    public float damage = 10f;
+    public float range = 100f;
+}
